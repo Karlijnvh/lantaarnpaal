a5e63fd8fcbe0422e83e4fe074707346c70987d8
Karlijnvh <112547700+Karlijnvh@users.noreply.github.com>
2024-03-04 14:34:07 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
+let toestand = 1
+let timerEindtijd = input.runningTime() + 5000
 basic.forever(function () {
-	
+    if (toestand == 1) {
+        if (input.lightLevel() < 100) {
+            if (input.runningTime() >= timerEindtijd) {
+                basic.showLeds(`
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    `)
+                timerEindtijd = input.runningTime() + 5000
+                toestand = 2
+            }
+        }
+    }
+    if (toestand == 2) {
+        if (input.lightLevel() >= 100) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            toestand = 1
+        }
+    }
 })
